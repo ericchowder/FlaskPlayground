b1e36aac5f41583eaeade06c0ee63b5ce0a12a8d
ericchowder <linksta7@gmail.com>
2021-04-19 23:55:12 -0400

    Created redirect page

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
 # render_template comes from "jinja"
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 # Passes env var name for app to initialize app
 app = Flask(__name__)
 
+# Home Page
 @app.route('/')
 def home():
     # Renders home.html template for this route
     # Passes variable "name" to template
 	return render_template('home.html', name="Eric")
 
+# About Page
 @app.route('/about')
 def about():
     return "This will be a URL shortener"
+
+# Redirect after shortening URL
+@app.route('/your-url')
+def your_url():
+    return render_template('your_url.html', code=request.args['code'])
