b642644bba08489251caa50bbb2ee368fd6148a6
ericchowder <linksta7@gmail.com>
2021-05-15 22:22:49 -0400

    Updated db to user env vars

--- playground/__init__.py
+++ playground/__init__.py
@@ -1,10 +1,17 @@
 from flask import Flask, Blueprint
 from flask_sqlalchemy import SQLAlchemy
+import sqlalchemy
 
 from .urlshort_bp import bp
 from .auth import auth
 from .users import users
 import os
+
+# Allow use of .env.local file
+from dotenv import load_dotenv
+
+# Load .env.local file and specify relative path
+load_dotenv(dotenv_path="./playground/.env.local") 
 
 
 def create_app(test_config=None):
@@ -18,12 +25,21 @@ def create_app(test_config=None):
 	app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 	# init SQLAlchemy so we can use it later in our models
-	db = SQLAlchemy()
+	db = SQLAlchemy(app)
+
+	class User(db.Model):
+		id = db.Column(db.Integer, primary_key=True)
+		public_id = db.Column(db.String(50), unique=True)
+		name = db.Column(db.String(50))
+		password = db.Column(db.String(80))
+		admin = db.Column(db.Boolean)
 
 	# Registers blueprint
 	app.register_blueprint(bp)
 	app.register_blueprint(auth, url_prefix='/auth')
 	app.register_blueprint(users)
+
+	db.create_all()
 
 	"""
 	Auth+DB tutorial stuff
@@ -44,3 +60,5 @@ def create_app(test_config=None):
 
 	return app
 
+# app = create_app()
+# db = SQLAlchemy(app)
