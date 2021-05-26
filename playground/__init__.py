74d3845f09cf40a59fd4dc5b490f7d69305d6785
ericchowder <linksta7@gmail.com>
2021-05-25 21:56:49 -0400

    Decoupled user routes out of init file

--- playground/__init__.py
+++ playground/__init__.py
@@ -1,13 +1,13 @@
-from flask import Flask, Blueprint, json, request, jsonify
+from flask import Flask, Blueprint
 from flask_sqlalchemy import SQLAlchemy
-import sqlalchemy
-import uuid
 from werkzeug.security import generate_password_hash, check_password_hash
-
+import sqlalchemy
+import os
+# Extensions
+from .database import db
 from .urlshort_bp import bp
 from .auth import auth
 from .users import users
-import os
 
 # Allow use of .env.local file
 from dotenv import load_dotenv
@@ -16,7 +16,7 @@ from dotenv import load_dotenv
 load_dotenv(dotenv_path="./playground/.env.local") 
 
 
-def create_app(test_config=None):
+def create_app(test_config='settings.py'):
 	# Passes env var name for app to initialize app
 	app = Flask(__name__)
 	app.secret_key = os.environ.get("SECRET_KEY", "")
@@ -25,22 +25,10 @@ def create_app(test_config=None):
 	app.config["SECRET_KEY"] = "mysecretkey"
 	app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("SQLITE_URI", "")
 	app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
+	app.config.from_pyfile(test_config)
 	# init SQLAlchemy so we can use it later in our models
-	db = SQLAlchemy(app)
+	db.init_app(app)
 
-	class User(db.Model):
-		id = db.Column(db.Integer, primary_key=True)
-		public_id = db.Column(db.String(50), unique=True)
-		name = db.Column(db.String(50))
-		password = db.Column(db.String(80))
-		admin = db.Column(db.Boolean)
-
-	class Todo(db.Model):
-		id = db.Column(db.Integer, primary_key=True)
-		text = db.Column(db.String(50))
-		complete = db.Column(db.Boolean)
-		user_id = db.Column(db.Integer)
 
 	##############
 	### ROUTES ###
@@ -50,81 +38,6 @@ def create_app(test_config=None):
 	def home():
 		return 'Hello World'
 
-	# Returns all existing users
-	@app.route('/user', methods=['GET'])
-	def get_all_users():
-		# Create a table of users to access
-		users = User.query.all()
-		# List of users to output
-		output = []
-		# Loop through users in users table
-		for user in users:
-			# Dictionary to store data of current user
-			user_data = {}
-			# Store each property (as specified from User class)
-			user_data['public_id'] = user.public_id
-			user_data['name'] = user.name
-			user_data['password'] = user.password
-			user_data['admin'] = user.admin
-			# Append current user to output list
-			output.append(user_data)
-		return jsonify({'users' : output})
-
-	# Returns one specified user
-	@app.route('/user/<public_id>', methods=['GET'])
-	def get_one_user(public_id):
-		# Query for first public id match
-		user = User.query.filter_by(public_id=public_id).first()
-		# If specified user does not exist
-		if not user:
-			return jsonify({'message' : 'No user found!'})
-		# If found, grab user poperty to return
-		user_data = {}
-		user_data['public_id'] = user.public_id
-		user_data['name'] = user.name
-		user_data['password'] = user.password
-		user_data['admin'] = user.admin
-		return jsonify({'user' : user_data})
-
-	@app.route('/user', methods=['POST'])
-	def create_user():
-		# Store request info into data
-		data = request.get_json()
-		# Hash new user's password (json request 'password' key)
-		hashed_password = generate_password_hash(data['password'], method='sha256')
-		# Instantiate new user
-		new_user = User(public_id=str(uuid.uuid4()), name=data['name'], password=hashed_password, admin=False)
-		# Add to db
-		db.session.add(new_user)
-		db.session.commit()
-		return jsonify({'message' : 'New user created!'})
-
-	@app.route('/user/<public_id>', methods=['PUT'])
-	def promote_user(public_id):
-		# Query for first public id match
-		user = User.query.filter_by(public_id=public_id).first()
-		# If specified user does not exist
-		if not user:
-			return jsonify({'message' : 'No user found!'})
-		# Set user as admin
-		user.admin = True
-		db.session.commit()
-		return jsonify({'message' : user.name + ' has been promoted'})
-
-	@app.route('/user/<public_id>', methods=['DELETE'])
-	def delete(public_id):
-		# Query for first public id match
-		user = User.query.filter_by(public_id=public_id).first()
-		# If specified user does not exist
-		if not user:
-			return jsonify({'message' : 'No user found!'})
-		name = user.name
-		# Delete specified user
-		db.session.delete(user)
-		db.session.commit()
-		return jsonify({'message' : name + ' has been deleted.'})
-	
-
 	###########################
 	### BLUE PRINT REGISTER ###
 	###########################
@@ -132,7 +45,8 @@ def create_app(test_config=None):
 	app.register_blueprint(auth, url_prefix='/auth')
 	app.register_blueprint(users)
 
-	db.create_all()
+	with app.app_context():
+		db.create_all()
 
 	"""
 	Auth+DB tutorial stuff
--- playground/users.py
+++ playground/users.py
@@ -1,7 +1,99 @@
-from flask import Blueprint
+from flask import Blueprint, json, request, jsonify
+from werkzeug.security import generate_password_hash, check_password_hash
+import uuid
+# Extensions
+from .database import db
 
 users = Blueprint('users', __name__)
 
-@users.route('/getusers')
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    public_id = db.Column(db.String(50), unique=True)
+    name = db.Column(db.String(50))
+    password = db.Column(db.String(80))
+    admin = db.Column(db.Boolean)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(50))
+    complete = db.Column(db.Boolean)
+    user_id = db.Column(db.Integer)
+
+@users.route('/login')
 def login():
-    return 'Users here!'
+    return 'login here!'
+
+# Returns all existing users
+@users.route('/user', methods=['GET'])
+def get_all_users():
+    # Create a table of users to access
+    users = User.query.all()
+    # List of users to output
+    output = []
+    # Loop through users in users table
+    for user in users:
+        # Dictionary to store data of current user
+        user_data = {}
+        # Store each property (as specified from User class)
+        user_data['public_id'] = user.public_id
+        user_data['name'] = user.name
+        user_data['password'] = user.password
+        user_data['admin'] = user.admin
+        # Append current user to output list
+        output.append(user_data)
+    return jsonify({'users' : output})
+
+# Returns one specified user
+@users.route('/user/<public_id>', methods=['GET'])
+def get_one_user(public_id):
+    # Query for first public id match
+    user = User.query.filter_by(public_id=public_id).first()
+    # If specified user does not exist
+    if not user:
+        return jsonify({'message' : 'No user found!'})
+    # If found, grab user poperty to return
+    user_data = {}
+    user_data['public_id'] = user.public_id
+    user_data['name'] = user.name
+    user_data['password'] = user.password
+    user_data['admin'] = user.admin
+    return jsonify({'user' : user_data})
+
+@users.route('/user', methods=['POST'])
+def create_user():
+    # Store request info into data
+    data = request.get_json()
+    # Hash new user's password (json request 'password' key)
+    hashed_password = generate_password_hash(data['password'], method='sha256')
+    # Instantiate new user
+    new_user = User(public_id=str(uuid.uuid4()), name=data['name'], password=hashed_password, admin=False)
+    # Add to db
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({'message' : 'New user created!'})
+
+@users.route('/user/<public_id>', methods=['PUT'])
+def promote_user(public_id):
+    # Query for first public id match
+    user = User.query.filter_by(public_id=public_id).first()
+    # If specified user does not exist
+    if not user:
+        return jsonify({'message' : 'No user found!'})
+    # Set user as admin
+    user.admin = True
+    db.session.commit()
+    return jsonify({'message' : user.name + ' has been promoted'})
+
+@users.route('/user/<public_id>', methods=['DELETE'])
+def delete(public_id):
+    # Query for first public id match
+    user = User.query.filter_by(public_id=public_id).first()
+    # If specified user does not exist
+    if not user:
+        return jsonify({'message' : 'No user found!'})
+    name = user.name
+    # Delete specified user
+    db.session.delete(user)
+    db.session.commit()
+    return jsonify({'message' : name + ' has been deleted.'})
+    
