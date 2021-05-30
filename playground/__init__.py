9fda464b20a31b279f44a4bfc032d40046abc8c6
ericchowder <linksta7@gmail.com>
2021-05-30 11:43:24 -0400

    cleaned up and re-organized init

--- playground/__init__.py
+++ playground/__init__.py
@@ -1,3 +1,6 @@
+######################
+### GLOBAL IMPORTS ###
+######################
 from flask import Flask
 import os
 
@@ -6,24 +9,21 @@ from dotenv import load_dotenv
 # Load .env.local file and specify relative path
 load_dotenv(dotenv_path="./playground/.env.local") 
 
+
+###################
+### APP CONFIGS ###
+###################
 # Passes env var name for app to initialize app
 app = Flask(__name__)
 app.secret_key = os.environ.get("SECRET_KEY", "")
 
-# Load Config based on environment
+# Load Config class based on environment var ENV i.e. FLASK_ENV
 if app.config["ENV"] == "production":
 	app.config.from_object('playground.config.ProductionConfig')
 if app.config["ENV"] == "testing":
 	app.config.from_object('playground.config.TestingConfig')
 else: #Development
 	app.config.from_object('playground.config.DevelopmentConfig')
-
-'''
-# SQL Config stuff
-app.config["SECRET_KEY"] = "mysecretkey"
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("SQLITE_URI", "")
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-'''
 
 
 ##############
@@ -34,24 +34,20 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 def home():
 	return 'Hello World'
 
+
+############################
+### IMPORTS / BLUEPRINTS ###
+############################
 from playground.database import db
 from playground.urlshort_bp import bp
 from playground.auth import auth
 from playground.users import users
-
-###########################
-### BLUE PRINT REGISTER ###
-###########################
 app.register_blueprint(bp)
 app.register_blueprint(auth, url_prefix='/auth')
 app.register_blueprint(users)
 
-from playground import test
-print("HELLO: " + test.hello)
-
 # init SQLAlchemy so we can use it later in our models
 db.init_app(app)
-
 with app.app_context():
 	db.create_all()
 
